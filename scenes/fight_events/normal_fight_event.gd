330847feb284800f32026c4f8a79c8c29ff909b9
MyAngelShibuyaKanon <99974252+MyAngelShibuyaKanon@users.noreply.github.com>
2024-01-30 00:27:19 +0800

    partial fight scene update

--- scenes/fight_events/normal_fight_event.gd
+++ scenes/fight_events/normal_fight_event.gd
@@ -31,7 +31,7 @@ func generate_question(enemy_type):
 	print(quest_data)
 	quest_type = quest_data.get("type")
 	var choices = quest_data.get("choices")
-	$Label.text = quest_data.get("question")
+	$TextPanel/Label.text = quest_data.get("question")
 	print(choices)
 	match quest_type:
 		1:
